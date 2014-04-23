4aba4bfe693ca56085fcd0650d37dfd87f5fac7a
Joe Germuska <joe@germuska.com>
2014-04-23 14:56:34 -0500

    misc sweeping

--- app/app.py
+++ app/app.py
@@ -3,10 +3,14 @@ from werkzeug import secure_filename
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html',title="Home Page")
-
+@app.route("/<template_name>")
+def index(template_name):
+    return render_template(template_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+# http://data.fao.org/developers/api/catalog/resource/findResourceTypes?version=1.0
+# http://data.fao.org/developers/api/catalog/resource/findResourceTypes
+# http://data.fao.org/developers/api/resources?version=1.0
+
--- app/fao-api.py
+++ app/fao-api.py
@@ -9,7 +9,22 @@ def _json_from_url(url):
         return r.json()
     r.raise_for_status()
 
+def walkit(items=None,indent=0):
+    if items is None:
+        j = resources()
+        items = j['result']['list']['items'] 
+    for i in items:
+        j = resources(parentType=i['type'])
+        print (' '*indent) + i['label'] + " [%s] (%i)" % (i['type'],len(j['result']['list']['items']))
+        if len(j['result']['list']['items']) != 0:
+            
+            walkit(j['result']['list']['items'],indent+2)
+
 def resources(**kwargs):
+    """Make a call to the resources API. Returns a JSON object where the good stuff is at
+       j['result']['list']['items']
+
+    """
     kwargs['version'] = VERSION
 
     return _json_from_url('http://data.fao.org/developers/api/resources?' + urlencode(kwargs))
