e92fdba73ca967829ce34dbfa6210761c25722ae
Zach Wise <zach@digitalartwork.net>
2014-05-03 14:58:02 -0400

    Added Shanghai image set

--- app/js/fao-explorer.js
+++ app/js/fao-explorer.js
@@ -23,43 +23,6 @@
 // @codekit-prepend "library/waypoints.js";
 // @codekit-prepend "library/waypoints-sticky.js";
 
-/*
-(function ($) {
-	
-	$(function(){
-
-		// fix sub nav on scroll
-		var $win = $(window),
-				$body = $('body'),
-				$nav = $('.subnav'),
-				navHeight = $('.navbar').first().height(),
-				subnavHeight = $('.subnav').first().height(),
-				subnavTop = $('.subnav').length && $('.subnav').offset().top - navHeight,
-				marginTop = parseInt($body.css('margin-top'), 10);
-				isFixed = 0;
-
-		processScroll();
-
-		$win.on('scroll', processScroll);
-
-		function processScroll() {
-			var i, scrollTop = $win.scrollTop();
-
-			if (scrollTop >= subnavTop && !isFixed) {
-				isFixed = 1;
-				$nav.addClass('navbar-fixed-top');
-				$body.css('margin-top', marginTop + subnavHeight + 'px');
-			} else if (scrollTop <= subnavTop && isFixed) {
-				isFixed = 0;
-				$nav.removeClass('navbar-fixed-top');
-				$body.css('margin-top', marginTop + 'px');
-			}
-		}
-
-	});
-
-})(window.jQuery);
-*/
 
 $(document).ready(function(){
 	
