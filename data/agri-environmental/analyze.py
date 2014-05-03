916504b17f224302be0d9756edd94281f737c627
Joe Germuska <joe@germuska.com>
2014-05-03 17:47:20 -0400

    tweaked to catch up with other changes

--- data/agri-environmental/analyze.py
+++ data/agri-environmental/analyze.py
@@ -1,11 +1,20 @@
 # some simple set up for analysis to avoid repetition
 import pandas as pd
+import numpy as np
+ISO_COUNTRY_CODES = {
+    'Brazil': 76,
+    'China': 156,
+    'India': 356,
+    'Nigeria': 566
+}
 
-# Country  UNI FAOSTAT
-# Brazil  76  21
-# China   156 351
-# India   356 100
-# Russia  643 185
+FAOSTAT_COUNTRY_CODES = {
+    'Brazil': 21,
+    'China': 351,
+    'India': 100,
+    'Nigeria': 159
+}
+
 
 ag = pd.read_csv('dump.csv')
 ag.rename(columns=dict(zip(ag.columns,['year', 'country', 'country_code', 'land_type', 'land_code', 'pct_ag_change', 'pct_ag_change_flg', 'pct_ag_area', 'pct_ag_area_flg', 'pct_land_area', 'pct_land_area_flg'])),inplace=True)
@@ -29,12 +38,12 @@ codes = codes.set_index('land_code')
 
 ag = ag.drop(['pct_ag_change_flg','pct_ag_area_flg','pct_land_area_flg'],axis=1)
 ag = ag[ag.year > 1983]
-ag = ag[(ag.country_code == 21) | (ag.country_code == 351) | (ag.country_code == 100 )]
+ag = ag[ag.country_code.isin(FAOSTAT_COUNTRY_CODES.values())]
 ag_area = ag[ag.land_code == 6610]
 agpiv = ag_area.pivot('year','country','pct_land_area')
 # agpiv.plot()
 
 brazil = ag[ag.country_code == 21]
 brazil = brazil[(brazil.land_code != 6610) | (brazil.land_code != 6700)]
-brag = brazil[isnan(brazil.pct_land_area)]
+brag = brazil[np.isnan(brazil.pct_land_area)]
 brag = brag.pivot('year','land_type','pct_ag_area')
